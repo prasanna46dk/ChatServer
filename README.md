0ce3d23deee8d1ef7f94749bbeba840070eda0fe
prasanna46dk <prasanna46dk@gmail.com>
2017-09-11 06:03:47 +0530

    comments added, files for future developement added, read me updated.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ChatServer
 c chat server 
+
+gcc server.c -o server
+gcc client.c -o client
+
+Run server first then run client from different terminal.
+
+./client <server_port_no> <client_name>.
--- client.c
+++ client.c
@@ -1,9 +1,9 @@
 #include <stdio.h> 
-#include <string.h>       //strlen 
+#include <string.h>       
 #include <stdlib.h> 
 #include <errno.h> 
-#include <unistd.h>       //close 
-#include <arpa/inet.h>    //close 
+#include <unistd.h>       
+#include <arpa/inet.h>    
 #include <sys/types.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
@@ -12,86 +12,16 @@
 #define OLDPEER 1
 #define NEWPEER 0
 #define STDIN 0
-typedef struct Packet{
-    unsigned int PacketNumber;
-    char *From;
-    char *To;
-    unsigned int LengthofMessage;
-    char *Message;      
-}Packet;
 
-Packet *ParsetoPacket(char *buff, int packetno, char *from){
-    Packet *packet = (Packet*)malloc(sizeof (Packet));
-    char *token = strtok(buff,"#");
-    int i = 0, length = 0;
-    while (token){
-        if ( i == 0){
-            packet -> To = token; 
-        } else if (i == 1){
-            packet -> Message = token;
-            packet -> LengthofMessage = strlen(token);
-            break;
-        }
-        i++;
-    }
-    packet -> PacketNumber = packetno;
-    packet -> From = from;
-    //strcpy(packet -> From, from); 
-    return packet;
-}
-
-int validateBuff(char *buff){
-    printf("Connected to server.\nPlease Enter user tio connect to:\n");
-    scanf("%[^\n]",buff);
-    char c;
-    scanf("%c",&c);
-    printf("%s\n",buff);
-    if (buff[1] == '@')
-       return OLDPEER;
-    else 
-       return NEWPEER;
-}
-
-//void TextUI(char *buff, char *DestUsername){
-//    printf("Connected to Server 127.0.0.1.\nPlease Enter client name to connect to :-\n");
-//    scanf("%s",DestUsername);
-//    printf("Please Enter message:\n");
-//    scanf("%[^\n]",buff);
-//    destAddress = buff;
-//    //char c;
-//    //scanf("%c",&c);
-//    printf("%s\n",buff);
-//}
-
-//char *CreateBuffer(char *SelfUserName, char *DestUsername, char *Message, char *bufferToBeSent) {
-//      len_source = strlen(SelfUserName);
-//      len_Dest = strlen(DestUsername);
-//      len_msg = strlen(Message);
-//      strcpy(bufferToBeSent, SelfUserName);
-//      bufferToBeSent[len_source] = "%";
-//      strcpy((bufferToBeSent+len_source+1), DestUsername);
-//          
-//} 
 int client(int port, char *name)
 {
     struct sockaddr_in address;
     int sock = 0, valread, length, activity, max_sd;
     struct sockaddr_in serv_addr;
     fd_set masterfd, readfds;
-    Packet *packet;
-    char *To;
-    char *hello = "Hello from client";
-    char c;
-    char DestUsername[15] = {'\0'};
-    char SelfUserName[15];
+    char c, DestUsername[15] = {'\0'}, SelfUserName[15], isClient[7] = {"client"}, message[976] = {'\0'}, msgDirection = '0', buffer[1024] = {'\0'}, readbuffer[1024] = {'\0'};
     strcpy(SelfUserName,name);
-    char isClient[7] = {"client"};
-    char message[976] = {'\0'};
-    char msgDirection = '0';
-    char buffer[1024] = {'\0'};
-    char readbuffer[1024] = {'\0'};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         printf("\n Socket creation error \n");
         return -1;
     }
@@ -102,15 +32,13 @@ int client(int port, char *name)
     serv_addr.sin_port = htons(port);
       
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-    {
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
         printf("\nInvalid address/ Address not supported \n");
 	close(sock);
         return -1;
     }
   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
 	close(sock);
         return -1;
@@ -143,31 +71,30 @@ int client(int port, char *name)
                 printf("\nReceived : %s\n",buffer);
                 printf("Enter destination username :\n");
             }
-        } //else {
-            scanf("%[^\n]",DestUsername);
-            scanf("%c",&c);
-            printf("Enter your Mesage: \n");
-            scanf("%[^\n]",message);
-            scanf("%c",&c);
-            strcpy(buffer,"#@#");
-            //src address
-            strcat(buffer,isClient);
-            strcat(buffer,"#");
-            strcat(buffer,name);
-            strcat(buffer,"#");
-            //dest address
-            strcat(buffer,DestUsername);
-            strcat(buffer,"#");
-            //client or server
-            //message
-            strcat(buffer,message);
-            strcat(buffer,"#");
-            //sending message
-            strcat(buffer,"0#@#");
-             
-            write(sock , buffer, sizeof(buffer));
-            printf("message sent\n");
-        //}
+        } 
+        scanf("%[^\n]",DestUsername);
+        scanf("%c",&c);
+        printf("Enter your Mesage: \n");
+        scanf("%[^\n]",message);
+        scanf("%c",&c);
+        strcpy(buffer,"#@#");
+        //client or server 
+        strcat(buffer,isClient);
+        strcat(buffer,"#");
+        //name of src client
+        strcat(buffer,name);
+        strcat(buffer,"#");
+        //name of dest client
+        strcat(buffer,DestUsername);
+        strcat(buffer,"#");
+        //message to send
+        strcat(buffer,message);
+        strcat(buffer,"#");
+        //direction of message
+        strcat(buffer,"0#@#");
+         
+        write(sock , buffer, sizeof(buffer));
+        printf("message sent\n");
     }
     return 0;
 }
